f3b65b22b2354d4cffa1e08643bde18bf96ca9a3
LEO CANTU <rl300126@rlas-macair-39.local>
2018-11-08 10:26:01 -0600

    Initial Commit

---
+++ gestureHackwich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gestureHackwich
+//
+//  Created by LEO CANTU on 11/8/18.
+//  Copyright © 2018 LEO CANTU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
